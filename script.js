a808fcc6dd2c31557be2279820dab70da5b4fb73
Josh Harrison <joshharrison@hotmail.co.uk>
2021-07-27 19:04:42 +0100

    Added function where when user adds book the form closes and books are displayed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let library = [];
 const container = document.querySelector(".container");
 const bookContainer = document.createElement("div");
 const addBookBtn = document.createElement("button");
-const form = document.getElementById("form")
+const form = document.getElementById("form");
 const closeAddBookBtn = document.createElement("button");
 const bookDiv = document.createElement("div");
 const titleInput = document.getElementById("titleinput");
@@ -10,13 +10,9 @@ const authorInput = document.getElementById("authorinput");
 const numberOfPagesInput = document.getElementById("nopinput");
 const hasBeenRead = document.getElementById("read");
 const notBeenRead = document.getElementById("notread");
-const addBookSubmitButton = document.getElementById("submitbook")
+const addBookSubmitButton = document.getElementById("submitbook");
 
 bookContainer.classList.add("bookcontainer");
-
-
-
-
 
 function Book(title, author, numberOfPages, read) {
   this.title = title;
@@ -51,93 +47,118 @@ displayBooks = () => {
   library.forEach((book, i) => {
     const bookDiv = document.createElement("div");
     bookDiv.classList.add("book");
-    bookDiv.innerText = `Title: ${book.title}, 
-        Author: ${book.author}, 
-        Number of Pages: ${book.numberOfPages}, 
+    bookDiv.innerText = `Title: ${book.title} 
+        Author: ${book.author} 
+        Number of Pages: ${book.numberOfPages} 
         Have i read the book?: ${book.read}`;
 
     //create delete button
-    const deleteButton = document.createElement("button")
-    deleteButton.innerText = "X"
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "X";
     deleteButton.addEventListener("click", () => {
-        library.splice(i,1);
-        console.log(library)
-        bookDiv.remove(i);
-        
+      library.splice(i, 1);
+      console.log(library);
+      bookDiv.remove(i);
+    });
+    //create read button
+    const readButton = document.createElement("button");
+    readButton.innerText = "Mark as read";
+    readButton.addEventListener("click", () => {
+      if (book.read) {
+        book.read = false;
+      } else {
+        book.read = true;
+      }
 
-    })
-    bookDiv.appendChild(deleteButton)
+      bookContainer.innerHTML = "";
+      displayBooks();
+
+      console.log(book);
+    });
+
+    bookDiv.appendChild(deleteButton);
+    bookDiv.appendChild(readButton);
     bookContainer.appendChild(bookDiv);
   });
   container.appendChild(bookContainer);
 };
 
-
 addBookToLibrary = () => {
   library.push(lordOfTheRings, harryPotter, joshBook);
   addBookBtn.textContent = "Add New Book";
   addBookBtn.classList.add("addbtn");
- 
-
 
   container.appendChild(addBookBtn);
 };
 
 showForm = () => {
-    addBookBtn.addEventListener("click", e => {
-        e.preventDefault();
-        form.classList.toggle("form")
-        form.classList.toggle("hidden")
-        bookContainer.style.opacity = 0
-        
-        
-      });
-}
-
-closeAddBookBtn.addEventListener("click", e => {
+  addBookBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    form.classList.toggle("form")
-    form.classList.toggle("hidden")
+    form.classList.toggle("form");
+    form.classList.toggle("hidden");
+    bookContainer.style.opacity = 0;
+  });
+};
+
+closeAddBookBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  form.classList.toggle("form");
+  form.classList.toggle("hidden");
+  bookContainer.innerHTML = "";
+  displayBooks();
+  clearForm();
+  bookContainer.style.opacity = 1;
+});
+
+getUserInputtedData = () => {
+  addBookSubmitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let title = form.elements[0].value;
+    let author = form.elements[1].value;
+    let noPages = form.elements[2].value;
+    let readOrNot = document.querySelector(
+      "input[name=hasbeenread]:checked"
+    ).value;
+    const newBook = new Book(
+      `${title}`,
+      `${author}`,
+      `${noPages}`,
+      `${readOrNot}`
+    );
+    library.push(newBook);
     bookContainer.innerHTML = "";
     displayBooks();
     clearForm();
-    bookContainer.style.opacity = 1
-    
-})
-
-
-getUserInputtedData = () => {
-    addBookSubmitButton.addEventListener("click", e => {
-        e.preventDefault();
-        let title = form.elements[0].value
-        let author = form.elements[1].value
-        let noPages = form.elements[2].value
-        let readOrNot = document.querySelector("input[name=hasbeenread]:checked").value;
-        const newBook = new Book(`${title}`, `${author}`, `${noPages}`,`${readOrNot}`);
-        library.push(newBook);
-        bookContainer.innerHTML = "";
-        displayBooks();
-        clearForm();
-        console.log(library)
-        console.log(newBook)
-        console.log(form.elements)
-    })
-}
+    //create functionality where when user submits the form, it closes
+    closeForm();
+    console.log(library);
+    console.log(newBook);
+    console.log(form.elements);
+  });
+};
 clearForm = () => {
-    form.elements[0].value = "";
-    form.elements[1].value = "";
-    form.elements[2].value = "";
-    form.elements[3].checked = false;
-    form.elements[4].checked = false;
-}
+  form.elements[0].value = "";
+  form.elements[1].value = "";
+  form.elements[2].value = "";
+  form.elements[3].checked = false;
+  form.elements[4].checked = false;
+};
+
+closeForm = () => {
+  form.classList.toggle("form");
+  form.classList.toggle("hidden");
+  bookContainer.innerHTML = "";
+  displayBooks();
+  clearForm();
+  bookContainer.style.opacity = 1;
+};
 
 
 
-closeAddBookBtn.innerText = "X"
+
+closeAddBookBtn.innerText = "X";
 closeAddBookBtn.classList.add("close");
 form.appendChild(closeAddBookBtn);
-
-
 
 addBookToLibrary();
 displayBooks();
